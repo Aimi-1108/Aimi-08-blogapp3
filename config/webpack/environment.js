462b25beb62f395be8e0b8566753edde922f3562
鈴木愛海 <>
2024-10-29 15:28:31 +0900

    webpacker.ymlとwebpacker/environment.jsにnodeの設定を追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
+// nodeオプションを追加
+environment.config.merge({
+  node: {
+    __dirname: false,
+    __filename: false,
+  }
+});
 module.exports = environment
