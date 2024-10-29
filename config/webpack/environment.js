645cebf84ba0febe451cfc1daf7ce72ea4415228
鈴木愛海 <>
2024-10-29 16:47:12 +0900

    global : trueの追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,12 @@
-
 const { environment } = require('@rails/webpacker');
 
+// 既存のenvironment設定にnodeオプションをマージ
 environment.config.merge({
-    node: {
-      __dirname: false,
-      __filename: false,
-      
-    }
-  });
-  
-module.exports = environment
+  node: {
+    __dirname: false,
+    __filename: false,
+    global: true, // globalプロパティを追加
+  }
+});
+
+module.exports = environment;
