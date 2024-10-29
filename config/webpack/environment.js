a45a8562f09967b7b8487492c1102a07ef87de93
鈴木愛海 <>
2024-10-29 16:42:52 +0900

    no message

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,12 @@
 
 const { environment } = require('@rails/webpacker');
 
-
+environment.config.merge({
+    node: {
+      __dirname: false,
+      __filename: false,
+      
+    }
+  });
+  
 module.exports = environment
