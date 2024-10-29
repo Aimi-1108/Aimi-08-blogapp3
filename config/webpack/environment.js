be4e0292db04112a30ffba660424aa0839cb82b2
鈴木愛海 <>
2024-10-29 15:49:30 +0900

    nodeオプションがymlとこのファイルに重複していたためこちらを削除

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,5 @@
-const { environment } = require('@rails/webpacker')
+
 const { environment } = require('@rails/webpacker');
 
-// nodeオプションを追加
-environment.config.merge({
-  node: {
-    __dirname: false,
-    __filename: false,
-  }
-});
+
 module.exports = environment
